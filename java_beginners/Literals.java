d14e4a19ab518fb0a1279c52c2d3ccc20f2f6b5e
Nikunj Patel <nikunjpatel1581996@gmail.com>
2024-03-13 00:11:58 +0530

    main data removed

--- java_beginners/Literals.java
+++
@@ -1,9 +0,0 @@
-public class Literals {
-    public static void main(String[] args) {
-        double nu1by = 10_00_000;
-        int nu2by = 10_00_000;
-        char a = 'a';
-        a++;
-        System.out.println(nu1by + "," + nu2by + "," + ++a);
-    }
-}
